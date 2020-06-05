59b75ac72a6ee8556a0517e28b16ef0750af2cdf
derek rosas <derekianrosas@gmail.com>
2020-06-04 22:33:49 -0400

    First file for project, nothing too crazy, this adds in the feature where user inputs cash

---
+++ casino.js
@@ -0,0 +1,7 @@
+console.log('Welcome to DiamondBack Casino');
+console.log("Home to our infamous slot machines, where you may or may not win something!");
+function moneyTracker() {
+    let totalCash = prompt("Let's get you started, how much money do you wanna risk?")
+    console.log(`Holy cow! ${totalCash}? that's a ton of money!`);
+    console.log("Don't worry, I won't ask twice!");
+}
